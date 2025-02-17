5feb24c4205ec0df0c9f9d42e07dbc6acb9bf9ac
Filip <radosta.filip@gmail.com>
2025-02-17 10:50:44 +0100

    add Zlomek class

--- src/main/java/ZlomkyApp.java
+++ src/main/java/ZlomkyApp.java
@@ -1,9 +1,14 @@
+import cz.uhk.zlomky.model.Zlomek;
+
 public class ZlomkyApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println("Zlomkoaplikace");
 
+        Zlomek a = new Zlomek(3, 4);
+
+        System.out.printf("Zlomek = %s", a.toString());
     }
 
 }
